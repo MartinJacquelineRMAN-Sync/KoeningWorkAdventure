02976daa020ca1752b7a6e5761a7b498531e49d7
Martin JACQUELINE <martin.jacqueline@rman-sync.com>
2021-10-22 13:53:17 +0200

    v0.2.2

--- tileset1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="tileset1" tilewidth="32" tileheight="32" tilecount="121" columns="11">
- <image source="src/assets/tileset1.png" width="352" height="352"/>
-</tileset>
--- tileset6_export.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="tileset6_export" tilewidth="32" tileheight="32" tilecount="100" columns="10">
- <image source="src/assets/tileset6_export.png" width="320" height="320"/>
-</tileset>
